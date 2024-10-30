073a38c9b86cab01390d862e43b1f33228654a26
Saif Tasnim <saiftasnim2002@gmail.com>
2024-10-30 11:00:14 +0600

    fix: page banner design

--- src/app/(public)/services/[slug]/component/PageBanner/PageBanner.tsx
+++ src/app/(public)/services/[slug]/component/PageBanner/PageBanner.tsx
@@ -9,10 +9,10 @@ const PageBanner: FC<IPageBanner> = ({ description, image, title }) => {
   return (
     <div className="flex justify-around items-center h-[90vh] w-full my-7">
       <div className="w-1/2">
-        <h1 className="text-3xl lg:text-6xl text-primary font-semibold ml-4 my-12">
+        <h1 className="text-3xl lg:text-6xl text-primary font-semibold lg:ml-4 my-12">
           {title}
         </h1>
-        <p className="ml-4 text-xl lg:text-2xl">{description}</p>
+        <p className="lg:ml-4 text-xl lg:text-2xl">{description}</p>
       </div>
 
       <div className="hidden lg:block">
